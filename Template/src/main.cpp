37bedb439280193c0b7a885ba006a1c6f779a4e8
JustinKin <justinkin2020@outlook.com>
2020-10-05 11:51:09 +0800

    template day1

--- Template/src/main.cpp
+++ Template/src/main.cpp
@@ -47,15 +47,34 @@ int main(int argc, char *avgv[])
     ostream_iterator<int> out_1(cout," "), out_2(cout,"\n");
     ostream_iterator<string>out_a(cout," "), out_b(cout,"\n");
 
+/*  int a = 3 , b = 9;
+    string s1 = "my", s2("compare");
+    auto result1 = mycompare(a,b);
+    auto result2 = mycompare(s1,s2);
+    auto result3 = mycompare(sizeof(s1),sizeof(s2));
+    auto result4 = mycompare(s1.size(),s2.size());
+
+    out_a = "mycompare(a,b) :"; out_2 = result1;
+    out_a = "mycompare(s1,s2):"; out_2 = result2;
+    out_a = "mycompare(sizeof(s1),sizeof(s2)):"; out_2 = result3;
+    out_a = "mycompare(s1.size(),s2.size()):"; out_2 = result4; */
 
 
+    vector<int> vec{1,2,3,4,5};
+    list<string> lst{"my","tem","m","template","nice"};
 
+    auto bg =vec.begin(), ed = vec.end();
+    auto r1 = myfind(bg, ed,3) - vec.begin();
+    auto r2 = myfind(bg, ed,6)- vec.begin();
 
+    auto bg2 =lst.begin(), ed2 = lst.end();
+    auto r3 = myfind(bg2, ed2, "my") ;
+    auto r4 = myfind(bg2, ed2, "m") ;
 
-
-
-
-
+    cout<<"myfind(bg, ed,3): "<<r1<<endl;
+    cout<<"myfind(bg, ed,6): "<<r2<<endl;
+    cout<<"myfind(bg, ed, \"my\"): "<<*r3<<endl;
+    cout<<"myfind(bg, ed, \"m\") "<<*r4<<endl;
 
 
 
