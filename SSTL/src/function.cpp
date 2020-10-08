b74df40e39c5fb543aa5df19b13a1c950db6eb96
JustinKin <justinkin2020@outlook.com>
2020-10-08 10:03:38 +0800

    SSTL

---
+++ SSTL/src/function.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <list>
+#include <forward_list>
+#include <algorithm>
+#include <functional>
+#include <iterator>
+#include <map>
+#include <set>
+#include <utility>
+#include <memory>
+
+#include "SSTL.H"
+
+using namespace std;
+
---
+++ SSTL/src/main.cpp
@@ -0,0 +1,64 @@
+/*
+                           _ooOoo_
+                          o8888888o
+                          88" . "88
+                          (| -_- |)
+                          O\  =  /O
+                       ____/`---'\____
+                     .'  \\|     |//  `.
+                    /  \\|||  :  |||//  \
+                   /  _||||| -:- |||||-  \
+                   |   | \\\  -  /// |   |
+                   | \_|  ''\---/''  |   |
+                   \  .-\__  `-`  ___/-. /
+                 ___`. .'  /--.--\  `. . __
+              ."" '<  `.___\_<|>_/___.'  >'"".
+             | | :  `- \`.;`\ _ /`;.`/ - ` : | |
+             \  \ `-.   \_ __\ /__ _/   .-` /  /
+        ======`-.____`-.___\_____/___.-`____.-'======
+                           `=---='
+        ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+        	一心向善	好运相伴	佛祖保佑	永无BUG
+*/
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <list>
+#include <forward_list>
+#include <algorithm>
+#include <functional>
+#include <iterator>
+#include <map>
+#include <set>
+#include <utility>
+#include <memory>
+// #include <conio.h>
+// #include <windows.h>
+#include "SSTL.H"
+
+using namespace std;
+
+
+int main(int argc, char *avgv[])
+{
+//=================================================================//
+    ostream_iterator<int> out_1(cout," "), out_2(cout,"\n");
+    ostream_iterator<string>out_a(cout," "), out_b(cout,"\n");
+
+
+
+
+
+
+
+
+
+
+//=================================================================//
+
+     system("pause");
+    return 0;
+}
+
