f6b0a24b649231bc4ba69605de1b77f77a1a58be
JustinKin <justinkin2020@outlook.com>
2020-10-10 11:42:32 +0800

    P652

--- SSTL/src/function.cpp
+++ SSTL/src/function.cpp
@@ -12,8 +12,21 @@
 #include <set>
 #include <utility>
 #include <memory>
-
+#include <random>
 #include "SSTL.H"
 
 using namespace std;
 
+
+vector<int> myrand()
+{
+    static default_random_engine e(608);
+	static uniform_int_distribution<int> u(-100,100);
+
+    vector<int> v;
+    for(size_t i = 0; i <10; ++i)
+        v.push_back(u(e));
+
+    return v;
+
+}
--- SSTL/src/main.cpp
+++ SSTL/src/main.cpp
@@ -34,6 +34,10 @@
 #include <set>
 #include <utility>
 #include <memory>
+#include <tuple>
+#include <regex>
+#include <random>
+
 // #include <conio.h>
 // #include <windows.h>
 #include "SSTL.H"
@@ -48,10 +52,44 @@ int main(int argc, char *avgv[])
     ostream_iterator<string>out_a(cout," "), out_b(cout,"\n");
 
 
+/* tuple<int,int,int> t(10,20,30);
+cout<<get<1>(t);
+
+tuple<string,vector<string>,pair<string,int>> svp;
+get<2>(svp) = {"hello", 2020};
+cout<<get<2>(svp).first<<" "<<get<2>(svp).second;
+cout<<"\n\n";
 
 
+default_random_engine e;
+uniform_int_distribution<unsigned> u(0,100);
 
+cout<<"min: "<<e.min()<<endl<<"max: "<<e.max()<<endl;
+for(size_t i = 0; i <10; ++i)
+{
+    cout<<u(e)<<" ";
+}
 
+char s;
+while(cin>>s)
+{
+    if(s =='x')
+        break;
+    for(const auto &c : myrand())
+        cout<<c<<" ";
+} */
+
+string pattern("[^c]ei");
+pattern = "[[:alpha:]]*" + pattern + "[[:alpha:]]*";
+regex r(pattern);
+smatch results;
+string test_str = "receipt freind theif receive";
+if(regex_search(test_str,results,r))
+    cout << results.str()<<"\n\n\n";
+
+regex r2(pattern,regex::icase);
+for(sregex_iterator it(test_str.begin(),test_str.end(),r), end_it; it!=end_it; ++it)
+    cout << it->str() <<endl;
 
 
 
